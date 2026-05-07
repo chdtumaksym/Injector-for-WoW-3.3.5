3bf90e3fcae77b7f90e6fb8585f62da19332a9c8
chdtumaksym <diversionka401@gmail.com>
2026-05-07 19:15:02 +0300

    Update payload.cpp

--- payload.cpp
+++ payload.cpp
@@ -1,15 +1,8 @@
-#include "main.h"
-
-// Наша главная функция внутри WoW
-DWORD WINAPI MainThread(LPVOID lpParam) {
-    // В будущем тут будет Object Manager и логика
-    MessageBoxA(NULL, "Инжект успешен! Warden нас не видит.", "Imba Bot", MB_OK);
-    return 0;
-}
+#include <windows.h>
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
-        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, nullptr, 0, nullptr);
+        MessageBoxA(NULL, "Payload Loaded!", "Success", MB_OK);
     }
     return TRUE;
 }
