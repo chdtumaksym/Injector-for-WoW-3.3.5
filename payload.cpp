178d6f6769b0c0ea7a4077ef6f1d6eea019f3d23
chdtumaksym <diversionka401@gmail.com>
2026-05-07 19:17:27 +0300

    Update payload.cpp

--- payload.cpp
+++ payload.cpp
@@ -1,8 +1,18 @@
 #include <windows.h>
 
+// Подключаем библиотеку для работы с окнами
+#pragma comment(lib, "user32.lib")
+
+DWORD WINAPI MainThread(LPVOID lpParam) {
+    // Сюда мы позже впишем ObjectManager и логику автобота
+    MessageBoxA(NULL, "Imba Bot запущен внутри WoW!", "Status", MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
+
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
-        MessageBoxA(NULL, "Payload Loaded!", "Success", MB_OK);
+        // Создаем поток, чтобы не вешать игру своей логикой
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MainThread, NULL, 0, NULL);
     }
     return TRUE;
 }
