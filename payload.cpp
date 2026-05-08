d9a58686aca256ba67d9ea306808767f36bfee5d
chdtumaksym <diversionka401@gmail.com>
2026-05-09 00:30:26 +0300

    Update payload.cpp

--- payload.cpp
+++ payload.cpp
@@ -10,9 +10,8 @@
 #define ADDR_MOUSEOVER_GUID     0x00BD07A0
 #define ADDR_LUA_EXECUTE        0x00819210
 
-// Константы ClickToMove для 3.3.5a
-#define CTM_LOOT                6
-#define CTM_ATTACK_GUID         11
+// Используем только Move (Тип 4). Никаких Attack или Loot для CTM!
+#define CTM_MOVE                4
 
 #pragma runtime_checks("", off)
 #pragma check_stack(off)
@@ -27,18 +26,23 @@ WNDPROC oWndProc = nullptr;
 struct CTM_BUFFER { uint64_t guid; float pos[3]; };
 static CTM_BUFFER g_ctm;
 
-// Безопасное выделение цели через встроенный Lua-движок
-void SelectTarget(uint64_t guid) {
-    // Подменяем GUID под мышкой
-    *(uint64_t*)ADDR_MOUSEOVER_GUID = guid;
-    // Заставляем игру саму взять это в таргет (обновляет UI, сервер и дескрипторы)
-    ((tLuaExecute)ADDR_LUA_EXECUTE)("TargetUnit('mouseover')", "bot_core", 0);
+// Безопасное выполнение Lua-кода
+void ExecuteLua(const char* command) {
+    ((tLuaExecute)ADDR_LUA_EXECUTE)(command, "bot_core", 0);
 }
 
-void SafeAction(uintptr_t pLocal, int type, uint64_t guid, float x, float y, float z) {
+// Захват цели через мышь (безопасно для движка)
+void SelectTarget(uint64_t guid) {
+    *(uint64_t*)ADDR_MOUSEOVER_GUID = guid;
+    ExecuteLua("TargetUnit('mouseover')");
+}
+
+// Безопасное передвижение (Без крашей UnitReaction)
+void SafeMove(uintptr_t pLocal, float x, float y, float z) {
     if (!pLocal) return;
-    g_ctm.guid = guid; g_ctm.pos[0] = x; g_ctm.pos[1] = y; g_ctm.pos[2] = z;
-    ((tClickToMove)ADDR_CLICK_TO_MOVE)(pLocal, type, &g_ctm.guid, g_ctm.pos, 0.5f);
+    g_ctm.guid = 0; // Для передвижения GUID должен быть 0!
+    g_ctm.pos[0] = x; g_ctm.pos[1] = y; g_ctm.pos[2] = z;
+    ((tClickToMove)ADDR_CLICK_TO_MOVE)(pLocal, CTM_MOVE, &g_ctm.guid, g_ctm.pos, 0.5f);
 }
 
 void BotPulse() {
@@ -52,7 +56,7 @@ void BotPulse() {
     uintptr_t pLocal = 0;
     float myX = 0, myY = 0, myZ = 0;
 
-    // Ищем локального игрока
+    // Ищем себя
     uintptr_t cur = *(uintptr_t*)(mgr + 0xAC);
     while (cur && (cur & 1) == 0) {
         if (*(uint64_t*)(cur + 0x30) == localGuid) {
@@ -76,7 +80,7 @@ void BotPulse() {
             uintptr_t desc = *(uintptr_t*)(cur + 0x8);
             if (desc) {
                 uint64_t objGuid = *(uint64_t*)(cur + 0x30);
-                if (objGuid != localGuid) { // Игнорируем себя
+                if (objGuid != localGuid) {
                     int hp = *(int*)(desc + 0x60);
                     uint32_t flags = *(uint32_t*)(desc + 0x114);
                     float tX = *(float*)(cur + 0x798);
@@ -88,7 +92,7 @@ void BotPulse() {
                             attackDist = dist; attackGuid = objGuid;
                             aPos[0] = tX; aPos[1] = tY; aPos[2] = *(float*)(cur + 0x7A0);
                         }
-                    } else if (hp <= 0 && (flags & 1)) { // Флаг 1 = Lootable
+                    } else if (hp <= 0 && (flags & 1)) {
                         if (dist < lootDist) {
                             lootDist = dist; lootGuid = objGuid;
                             lPos[0] = tX; lPos[1] = tY; lPos[2] = *(float*)(cur + 0x7A0);
@@ -101,23 +105,35 @@ void BotPulse() {
     }
 
     static DWORD lastAction = 0;
+    // Тик раз в 500мс
     if (GetTickCount() - lastAction > 500) {
-        if (lootGuid && lootDist < 5.0f) {
-            SelectTarget(lootGuid); // Сначала безопасно берем в таргет
-            SafeAction(pLocal, CTM_LOOT, lootGuid, lPos[0], lPos[1], lPos[2]);
-            printf("Looting: %llu               \r", lootGuid);
+        
+        // ПРИОРИТЕТ 1: ЛУТ
+        if (lootGuid) {
+            if (lootDist > 4.5f) {
+                // Если далеко - просто бежим туда (CTM Type 4)
+                SafeMove(pLocal, lPos[0], lPos[1], lPos[2]);
+                printf("Moving to Loot... Dist: %.1f    \r", lootDist);
+            } else {
+                // Если близко - лутаем через безопасный Lua API
+                SelectTarget(lootGuid);
+                ExecuteLua("InteractUnit('target')");
+                printf("Looting!                        \r");
+            }
             lastAction = GetTickCount();
         } 
+        // ПРИОРИТЕТ 2: АТАКА
         else if (attackGuid) {
-            SelectTarget(attackGuid); // Синхронизируем дескрипторы для движка
-            SafeAction(pLocal, CTM_ATTACK_GUID, attackGuid, aPos[0], aPos[1], aPos[2]);
-            printf("Attacking: %llu | Dist: %.1f  \r", attackGuid, attackDist);
-            lastAction = GetTickCount();
-        } 
-        else if (lootGuid) {
-            SelectTarget(lootGuid);
-            SafeAction(pLocal, CTM_LOOT, lootGuid, lPos[0], lPos[1], lPos[2]);
-            printf("Running to Loot: %llu       \r", lootGuid);
+            if (attackDist > 4.5f) {
+                // Бежим к врагу
+                SafeMove(pLocal, aPos[0], aPos[1], aPos[2]);
+                printf("Moving to Target... Dist: %.1f  \r", attackDist);
+            } else {
+                // Бьем врага через Lua
+                SelectTarget(attackGuid);
+                ExecuteLua("AttackTarget()");
+                printf("Attacking!                      \r");
+            }
             lastAction = GetTickCount();
         }
     }
@@ -137,7 +153,7 @@ LRESULT CALLBACK HookedWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPara
 
 DWORD WINAPI Setup(LPVOID) {
     AllocConsole(); freopen("CONOUT$", "w", stdout);
-    printf("--- Bot v121: Hybrid Engine (WndProc + Lua) ---\n");
+    printf("--- Bot v122: Safe-Mode Taxi Engine ---\n");
 
     HWND hwnd = FindWindowA(NULL, "World of Warcraft");
     if (!hwnd) {
@@ -148,9 +164,8 @@ DWORD WINAPI Setup(LPVOID) {
     oWndProc = (WNDPROC)SetWindowLongA(hwnd, GWL_WNDPROC, (LONG)HookedWndProc);
     SetTimer(hwnd, 1337, 500, NULL);
 
-    printf("[+] Setup Complete.\n");
-    printf("[!] Click-to-Move MUST be enabled in game settings.\n");
-    printf("[+] Press [INSERT] to start farming.\n");
+    printf("[+] Setup Complete. Crash-proof logic activated.\n");
+    printf("[+] Focus WoW window and press [INSERT] to start farming.\n");
     return 0;
 }
 
