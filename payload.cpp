c3a112c550220ab9929fd4e21ba1e6c8bf967537
chdtumaksym <diversionka401@gmail.com>
2026-05-08 18:04:49 +0300

    Update payload.cpp

--- payload.cpp
+++ payload.cpp
@@ -1,60 +1,49 @@
 #include <windows.h>
 #include <iostream>
-#include <cmath>
-#include <vector>
+#include <stdint.h>
 
-struct BotCommand {
-    volatile int action;
-    volatile uint64_t guid;
-    volatile float x, y, z;
-} g_Cmd;
-
-template<typename T>
-T SafeRead(uintptr_t addr) {
-    T val{};
-    __try { val = *(T*)addr; } __except(1) {}
-    return val;
+template <typename T>
+T SafeRead(uintptr_t address) {
+    T buffer = T();
+    if (!address) return buffer;
+    __try { buffer = *(T*)address; } __except(1) {}
+    return buffer;
 }
 
-template<typename T>
-void SafeWrite(uintptr_t addr, T val) {
-    if (!addr) return;
-    __try { *(T*)addr = val; } __except(1) {}
+template <typename T>
+void SafeWrite(uintptr_t address, T value) {
+    if (!address) return;
+    __try { *(T*)address = value; } __except(1) {}
 }
 
-DWORD WINAPI BotThread(LPVOID lpParam) {
-    uintptr_t baseAddr = (uintptr_t)GetModuleHandleA("WoW.exe");
-    while (!SafeRead<uintptr_t>(baseAddr + 0x00879CE0)) Sleep(100); // ждём Object Manager
+DWORD WINAPI MainThread(LPVOID lpParam) {
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    std::cout << "--- Skeleton Payload Loaded ---" << std::endl;
 
     while (!GetAsyncKeyState(VK_END)) {
-        uintptr_t mgr = SafeRead<uintptr_t>(baseAddr + 0x00879CE0 + 0x2ED0);
-        uintptr_t pLocal = 0;
-        if (mgr) {
-            uintptr_t cur = SafeRead<uintptr_t>(mgr + 0xAC);
-            uint64_t myGuid = SafeRead<uint64_t>(mgr + 0xC0);
-            while (cur && (cur & 1) == 0) {
-                if (SafeRead<uint64_t>(cur + 0x30) == myGuid) { pLocal = cur; break; }
-                cur = SafeRead<uintptr_t>(cur + 0x3C);
-            }
+        // Пример чтения координат игрока
+        uintptr_t playerBase = SafeRead<uintptr_t>(0x00BD07E0); // проверять актуальный оффсет для билда
+        if (playerBase) {
+            float x = SafeRead<float>(playerBase + 0x798);
+            float y = SafeRead<float>(playerBase + 0x79C);
+            float z = SafeRead<float>(playerBase + 0x7A0);
+            std::cout << "Player pos: X=" << x << " Y=" << y << " Z=" << z << "\r";
         }
-        if (pLocal) {
-            // безопасная логика движения/атаки
-            if (g_Cmd.action) {
-                float pos[3] = { g_Cmd.x, g_Cmd.y, g_Cmd.z };
-                uintptr_t pDesc = SafeRead<uintptr_t>(pLocal + 0x8);
-                if (pDesc) SafeWrite<float>(pLocal + 0x798, pos[0]); // пример безопасного write
-                g_Cmd.action = 0;
-            }
-        }
-        Sleep(50);
+        Sleep(500);
     }
+
+    fclose(f);
+    FreeConsole();
+    FreeLibraryAndExitThread((HMODULE)lpParam, 0);
     return 0;
 }
 
-BOOL WINAPI DllMain(HINSTANCE hInst, DWORD reason, LPVOID) {
-    if (reason == DLL_PROCESS_ATTACH) {
-        DisableThreadLibraryCalls(hInst);
-        CreateThread(0, 0, BotThread, hInst, 0, 0);
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(hinstDLL);
+        CreateThread(nullptr, 0, MainThread, hinstDLL, 0, nullptr);
     }
     return TRUE;
 }
